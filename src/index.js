30b34c4e80f6c9a0c0cc3465f6313baaca3756b7
SupermanWY <1055190598@qq.com>
2021-02-12 02:47:00 +0800

    vconsile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import  Home from './views/home'
 import 'antd-mobile/dist/antd-mobile.css'
+import VConsole from 'vconsole'
+
+new VConsole();
 
 ReactDOM.render(
   <React.StrictMode>

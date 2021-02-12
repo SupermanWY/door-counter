1509bace6c2cd3ec4c298f86abb0a7ca8802a180
SupermanWY <1055190598@qq.com>
2021-02-12 13:53:19 +0800

    去除vconsole

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import  Home from './views/home'
 import 'antd-mobile/dist/antd-mobile.css'
-import VConsole from 'vconsole'
-
-new VConsole();
 
 ReactDOM.render(
   <React.StrictMode>
